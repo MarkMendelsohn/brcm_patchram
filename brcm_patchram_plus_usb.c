0667ff173d2264149ec24e17fcb143ef7ac81d3e
Ayan George <ayan.george@canonical.com>
2012-06-01 12:28:27 -0400

    Include strings.h to quiet compiler warnings.

--- brcm_patchram_plus_usb.c
+++ brcm_patchram_plus_usb.c
@@ -49,6 +49,7 @@
 ******************************************************************************/
 #include <stdio.h>
 #include <getopt.h>
+#include <strings.h>
 #include <errno.h>
 
 #include <sys/types.h>
